08f363cf8db4a8b7ab4290d2aef8c94843625ee5
lamees-nd <lameesn@equitick.com>
2024-07-10 16:19:33 +0300

    Animated Mobile Menu & Hamburger Icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./build/*.html","./build/*.js"],
   theme: {
     extend: {
       screens: {
@@ -8,8 +8,18 @@ module.exports = {
           raw: "(min-aspect-ratio: 3/2)",
         },
         tallscreen: {
-          raw: "(min-aspect-ratio: 1/2)",
+          raw: "(min-aspect-ratio: 13/20)",
         },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
       },
     },
   },
